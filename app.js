a65e6f351cb3f88e8197981451c13e07c27d3515
aploftus <aploftus@users.noreply.github.com>
2017-10-25 10:44:42 -0700

    Generate GroceryList from item Components

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+var Bananas = () => (
+  <li>bananas</li>
+);
+
+var Tomatoes = () => (
+  <li>tomatoes</li>
+);
+
+var GroceryList = () => (
+  <ul>
+    <Bananas />
+    <Tomatoes />
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
