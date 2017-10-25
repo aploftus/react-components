58721cf697f57a003dc956efdcf4bf43113d0ce0
aploftus <aploftus@users.noreply.github.com>
2017-10-25 11:04:35 -0700

    Render one GroceryListItem using props

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 // TODO
-var Bananas = () => (
-  <li>bananas</li>
+
+// Here we create a `TodoList` component
+var TodoList = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
 );
 
-var Tomatoes = () => (
-  <li>tomatoes</li>
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+  </div>
 );
+// var Bananas = () => (
+//   <li>bananas</li>
+// );
+
+// var Tomatoes = () => (
+//   <li>tomatoes</li>
+// );
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+var groceryItems = ['banana', 'apple', 'orange'];
 
 var GroceryList = () => (
   <ul>
-    <Bananas />
-    <Tomatoes />
+    <GroceryListItem item = {'banana'} />
   </ul>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
