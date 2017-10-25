a4686dbe697a94a602190ec2a48ecfbf257bc0e3
aploftus <aploftus@users.noreply.github.com>
2017-10-25 11:32:58 -0700

    Instantiate GroceryListItem as a subclass component

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-// Here we create a `TodoList` component
-var TodoList = (props) => (
-  <ul>
-    <li>{props.todos[0]}</li>
-    <li>{props.todos[1]}</li>
-    <li>{props.todos[2]}</li>
-  </ul>
-);
+  render() {
+    return (
+      <li>{this.props.item}</li>
+    );
+  }
+};
 
-var App = () => (
-  <div>
-    <h2>My Todo List</h2>
-    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
-  </div>
-);
-// var Bananas = () => (
-//   <li>bananas</li>
-// );
-
-// var Tomatoes = () => (
-//   <li>tomatoes</li>
-// );
-
-const GroceryListItem = (props) => (
-  <li>{props.item}</li>
-);
-
-let groceryItems = ['banana', 'apple', 'orange'];
+let groceryItems = ['banana', 'apple', 'orange', 'carrot'];
 
 const GroceryList = (props) => {
   const groceryItems = props.items.map(item => <GroceryListItem item = {item} />);
 
-  return ( <ul>
-    {groceryItems}
-  </ul> )
+  return (
+    <ul>
+      {groceryItems}
+    </ul>
+  );
 };
 
 ReactDOM.render(<GroceryList items = {groceryItems} />, document.getElementById("app"));
-// ReactDOM.render(<GroceryList />, document.getElementById("app"));
